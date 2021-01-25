ac0ea2dd88fe2b952b519349ef68519e85921ec2
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-01-25 16:17:50 +0100

    Esercizio parallelo con Vue
    1 - Creo il file index_vue.php
    2 - Installo vue dalla powershell
    3 - Creo il file app.js tramite il webpack.mix.js
    4 - Lancio npx mix e collego il mio file js in fondo al broswer

---
+++ index_vue.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- font awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.css" integrity="sha512-9iWaz7iMchMkQOKA8K4Qpz6bpQRbhedFJB+MSdmJ5Nf4qIN1+5wOVnzg5BQs/mYH3sKtzY+DOgxiwMz8ZtMCsw==" crossorigin="anonymous" />
+  <!-- font -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <!-- css -->
+  <link rel="stylesheet" href="css/app.css">
+  <title>Versione VUE - php-ajax-dischi</title>
+</head>
+
+<body>
+  <div id="app">
+  
+  <!-- header -->
+  <?php require __DIR__ . "/php_partials/header.php"; ?>
+  <!-- /header -->
+
+  <!-- main -->
+  <main>
+    <h1>Versione VUE - php-ajax-dischi</h1>
+    <!-- CODICE QUA -->
+  </main>
+  <!-- /main -->
+
+  <!-- footer -->
+  <?php include __DIR__ . "/php_partials/footer.php"; ?>
+  <!-- /footer -->
+  
+  </div>
+  <script src="js/app.js" charset="utf-8"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 let mix = require('laravel-mix');
-mix.sass('src/app.scss', 'css').options({
+
+mix
+.js('src/app.js', 'js').vue({ version: 2 })
+.sass('src/app.scss', 'css').options({
     processCssUrls: false // questa opzione fa in modo da non modificare i percorsi delle immagini
 });
