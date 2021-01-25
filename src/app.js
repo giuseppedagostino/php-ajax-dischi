73c9f88916f5ada6352ad98c0949aa42ac31f47d
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-01-25 17:05:22 +0100

    Chiamata axios
    1 - Installo axios dalla powershell
    2 - Lo importo nel file js e faccio debug stampando la chiamata get nella console

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
+// importo vue
 import Vue from 'vue';
+// importo axios
+const axios = require('axios').default;
 
 var app = new Vue({
   el: "#app",
-  data: {
-    
-  },
+  data: {},
   mounted: function() {
     alert("Vue funziona!");
+    axios
+    // non devo mettere i puntini e lo slash perchè a quanto pare parte da file index_vue.php, che si trova "un livello sopra"
+    // inoltre devo chiamare il server perchè è il file dove l'array php viene convertito in uno leggibile da js
+    .get("php_partials/server.php")
+    .then( function(response) {
+      console.log(response.data);
+    });
   }
 });
